6300b4cbc30c8ada786b24b27ec0ea791c92d881
Jinang Dhiraj Oswal <92290211+Jinang123@users.noreply.github.com>
2021-10-17 18:37:36 +0530

    Add files via upload

---
+++ change the case without using strupr or lwr assign 1 part4.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include <string.h>
+
+
+int main()
+{
+    char s[1000],*p;
+    int i;
+
+    printf("Enter  the string: ");
+    gets(s);
+
+    p=s;
+
+    i=0;
+	while(*(p+i))
+    {
+        if(*(p+i)>=65 && *(p+i)<=90)
+         *(p+i)+=32;
+        else if(*(p+i)>=97 && *(p+i)<=122)
+         *(p+i)-=32;
+
+        i++;
+ 	}
+
+    printf("string in changes case is as follows ='%s'\n",p);
+
+
+
+}
